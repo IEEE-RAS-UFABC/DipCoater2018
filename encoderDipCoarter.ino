9c4246488b7d0d3f56ea3d00819b1b666efe714d
Leonardo LF <32395899+ElmerLeo@users.noreply.github.com>
2018-07-23 17:01:27 -0300

    Comentando melhor o codigo.
    Talvez precisemos alterar a função menu para trabalhar com esse método de controle, mas deve ser uma mudança pequena

--- encoderDipCoarter.ino
+++ encoderDipCoarter.ino
@@ -10,7 +10,7 @@
 
 
 
-const int encoderPino1 = 5;
+const int encoderPino1 = 5; //mudar para os pinos que serao usados
 const int encoderPino2 = 6;
 
 void setup() {
@@ -23,20 +23,22 @@ void loop() {
 }
 
 int encoder() {
-  static bool oldEstado1;
-  static bool oldEstado2;
-  bool estado1;
+  static bool oldEstado1; //estados anteriores dos pinos do encoder
+  static bool oldEstado2; //variaveis com o prefixo static tem seus dados mantidos mesmo quando a função acaba
+  bool estado1; //novos estados dos pinos
   bool estado2;
 
+  //leitura dos novos estados dos pinos
   estado1 = digitalRead(encoderPino1);
   estado2 = digitalRead(encoderPino2);
   
+  //estados novo e comparado com o antigo
   if(estado1 == oldEstado1){
     if(oldEstado2 == LOW && estado2 == HIGH){
-      return 1;
+      return 1; //caso rotacao horaria
     }
     if(oldEstado2 == HIGH && estado2 == LOW){
-      return -1;
+      return -1; //caso rotacao antihoraria
     }
   }
   if(estado2 == oldEstado2){
@@ -48,10 +50,10 @@ int encoder() {
     }
   }
   else{
-    return 0;
+    return 0; //caso sem mudanca de estado
   }
   
-  oldEstado1 = estado1;
+  oldEstado1 = estado1; //estados sao salvos como antigos
   oldEstado2 = estado2;
 
 }

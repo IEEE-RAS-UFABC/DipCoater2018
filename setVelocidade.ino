5b7616a5e9960bd06d0b1d4b6b708dec11e4d6c2
Leonardo LF <32395899+ElmerLeo@users.noreply.github.com>
2018-07-24 11:07:06 -0300

    função setVelocidade

---
+++ setVelocidade.ino
@@ -0,0 +1,39 @@
+#include <LiquidCrystal.h> //Em teoria essas 4 coisas 
+LiquidCrystal lcd(2,3,4,5,6,7); //aqui já existem no 
+const int botaoSelecionar = 9; //programa do menu
+const int botaoVoltar = 10;
+
+int velocidadeDeFuncionamento = 0; //esse deve ser o mesmo usado 
+//nas funções do que vao girar os motores de passo
+
+int encoder(); //chamando a funcao encoder
+
+void setup() {
+}
+
+void loop(){  
+}
+
+
+void velocidade(){
+  int velocidadeI = 0;
+  bool rodando = LOW;
+  while(rodando == LOW){
+    velocidadeI = velocidadeI + encoder();
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Set velocidade");
+    lcd.setCursor(0, 1);
+    lcd.print("(cm/min)");
+    lcd.setCursor(10,10);
+    lcd.print(velocidadeI, DEC);
+    delay(200);
+    if(digitalRead(botaoSelecionar)){
+      rodando = HIGH;
+      velocidadeDeFuncionamento = velocidadeI;
+    }
+    if(digitalRead(botaoVoltar)){
+      rodando = HIGH;
+    }
+  }
+}

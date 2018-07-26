0348583fd72111764e8f995213759c814e3a6202
Leonardo LF <32395899+ElmerLeo@users.noreply.github.com>
2018-07-25 22:20:47 -0300

    Codigo do Encoder adicionado no menu
    O código já foi adaptado pra mudar as linhas do display pelo encoder

--- Menu.ino
+++ Menu.ino
@@ -10,6 +10,16 @@ char* beckers [] = {"Becker 1",  "Becker 2", "Becker 3" , "Becker 4"};
 int variavel [] = {1, 2, 3, 4, 5,6,7};
 int opt;
 
+int	velocidadeDeFuncionamento = 20; //inicianto variaveis de Funcionamento em valores seguros
+int 	tempoDeFuncionamento = 2;
+int	ciclosDeFuncionamento = 2;
+int	semovido = 0;
+
+
+const int encoderPino1 = 8; //pinos usados pelo encoder
+const int encoderPino2 = 9;
+int encoder(); //quando chamado te devolve se ouve rotação pra esquerda(-1) ou pra direita(1) ou 0
+
 String readOption = "Start Padrao";
 
 // initialize the library with the numbers of the interface pins
@@ -22,8 +32,11 @@ bool voltar;
 
 void setup() {
   // set up the LCD's number of columns and rows:
-   Serial.begin(9600);
+  Serial.begin(9600);
   lcd.begin(16, 3);
+	
+  pinMode(encoderPino1, INPUT);
+  pinMode(encoderPino2, INPUT);
   
 }
 
@@ -60,19 +73,24 @@ void loop(){
         }
         
       }
-      else{  
-        lcd.clear();
-        tecla_pressionada = ' ';
+ }
+  semovido = encoder();
+  if(semovido != 0){
+     linha = linha + semovido;
+     semovido=0;
+     lcd.clear();
+	  if(linha>1){
+		  linha = 0;
+	  }
+	  if(linha<0){
+		  linha = 1;
+	  }
+  }
         
-        if(linha <1){ //Faz cursor alterar entre linha 1 e linha 2
-          linha++;
-        }
-        else {linha =0;}
-        
-        //atribui a opcao escolhida à linha que esta o cursor
-        readOption = menu[linha];
-      }
-    }
+  //atribui a opcao escolhida à linha que esta o cursor
+  readOption = menu[linha];
+      
+    
 }
 
 void Submenu(bool voltar){
@@ -207,4 +225,40 @@ void funcTempo(){
 void funcPadrao(){
   Serial.print("Inicia a função PADRAO");
   //código para setar os diferentes tipos de padrões pre configurados (3 padrões)
+}
+
+int encoder() {
+  static bool oldEstado1; //estados anteriores dos pinos do encoder
+  static bool oldEstado2; //variaveis com o prefixo static tem seus dados mantidos mesmo quando a função acaba
+  bool estado1; //novos estados dos pinos
+  bool estado2;
+
+  //leitura dos novos estados dos pinos
+  estado1 = digitalRead(encoderPino1);
+  estado2 = digitalRead(encoderPino2);
+  
+  //estados novo e comparado com o antigo
+  if(estado1 == oldEstado1){
+    if(oldEstado2 == LOW && estado2 == HIGH){
+      return 1; //caso rotacao horaria
+    }
+    if(oldEstado2 == HIGH && estado2 == LOW){
+      return -1; //caso rotacao antihoraria
+    }
+  }
+  if(estado2 == oldEstado2){
+    if(oldEstado1 == LOW && estado1 == HIGH){
+      return 1;
+    }
+    if(oldEstado1 == HIGH && estado1 == LOW){
+      return -1;
+    }
+  }
+  else{
+    return 0; //caso sem mudanca de estado
+  }
+  
+  oldEstado1 = estado1; //estados sao salvos como antigos
+  oldEstado2 = estado2;
+
 }

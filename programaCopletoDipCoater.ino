10f4340d33f0331f86b0ece32e2feb66e03f32fe
Leonardo LF <32395899+ElmerLeo@users.noreply.github.com>
2018-08-27 19:26:21 -0300

    Arrumando a função encoder

--- programaCopletoDipCoater.ino
+++ programaCopletoDipCoater.ino
@@ -415,16 +415,14 @@ int encoder() {
   static bool oldEstado2; //variaveis com o prefixo static tem seus dados mantidos mesmo quando a função acaba
   bool estado1; //novos estados dos pinos
   bool estado2;
-
+  while(digitalRead(encoderPino1) == LOW && igitalRead(encoderPino2) == LOW){
   //leitura dos novos estados dos pinos
   //Serial.println("testando encoder");
   estado1 = digitalRead(encoderPino1);
   estado2 = digitalRead(encoderPino2);
   //Serial.println(estado1);
-  //Serial.println(estado2);
-  
-  //estados novo e comparado com o antigo
-   if(oldEstado1 == HIGH && oldEstado2 == HIGH && estado1 == LOW && estado2 == LOW){
+  //Serial.println(estado2); 
+   if(oldEstado1 == LOW && oldEstado2 == HIGH && estado1 == LOW && estado2 == LOW){
      oldEstado1 = estado1;
      oldEstado2 = estado2;
      Serial.println(estado1);
@@ -444,7 +442,7 @@ int encoder() {
      }
   oldEstado1 = estado1; 
   oldEstado2 = estado2;
-  
+  }
   return 0; //caso sem mudanca de estado
 }
 

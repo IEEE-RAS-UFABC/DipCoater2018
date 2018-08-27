daad201be3637d6d4574b26ac4566e3005637d08
Leonardo LF <32395899+ElmerLeo@users.noreply.github.com>
2018-08-27 19:49:49 -0300

    Atualizando pra ficar igual a do codigo principal

--- encoderDipCoarter.ino
+++ encoderDipCoarter.ino
@@ -14,12 +14,13 @@ const int encoderPino1 = 5; //mudar para os pinos que serao usados
 const int encoderPino2 = 6;
 
 void setup() {
+  Serial.begin(9600);
   pinMode(encoderPino1, INPUT);
   pinMode(encoderPino2, INPUT);
 }
 
 void loop() {
-
+Serial.print(encoder());
 }
 
 int encoder() {
@@ -27,65 +28,33 @@ int encoder() {
   static bool oldEstado2; //variaveis com o prefixo static tem seus dados mantidos mesmo quando a função acaba
   bool estado1; //novos estados dos pinos
   bool estado2;
-
+  while(not(digitalRead(encoderPino1) == HIGH || igitalRead(encoderPino2) == HIGH)){
   //leitura dos novos estados dos pinos
+  //Serial.println("testando encoder");
   estado1 = digitalRead(encoderPino1);
   estado2 = digitalRead(encoderPino2);
-  
-  //estados novo e comparado com o antigo
-  if(estado1 == oldEstado1){
-    if(estado1 == HIGH){
-    	if(oldEstado2 == LOW && estado2 == HIGH){
-     	 oldEstado1 = estado1;
-  	 	 oldEstado2 = estado2;
-     	 return 1; //caso rotacao horaria
-    	}
-    	if(oldEstado2 == HIGH && estado2 == LOW){
-      	oldEstado1 = estado1;
-  	  	oldEstado2 = estado2;
-      	return -1; //caso rotacao antihoraria
-        }
-    }
-    else{
-    	if(oldEstado2 == LOW && estado2 == HIGH){
-     	 oldEstado1 = estado1;
-  	 	 oldEstado2 = estado2;
-     	 return -1; //caso rotacao horaria
-    	}
-    	if(oldEstado2 == HIGH && estado2 == LOW){
-      	oldEstado1 = estado1;
-  	  	oldEstado2 = estado2;
-      	return 1; //caso rotacao antihoraria
-        }
-    }
-  }
-  if(estado2 == oldEstado2){
-    if(estado2 == HIGH){
-    	if(oldEstado1 == LOW && estado1 == HIGH){
-    	  oldEstado1 = estado1;
-    	  oldEstado2 = estado2;
-      	  return -1;
-    	}
-    	if(oldEstado1 == HIGH && estado1 == LOW){
-      		oldEstado1 = estado1;
-  	  		oldEstado2 = estado2;
-      	return 1;
-        }     
-    }
-    else{
-    	if(oldEstado1 == LOW && estado1 == HIGH){
-    	  oldEstado1 = estado1;
-    	  oldEstado2 = estado2;
-      	  return 1;
-    	}
-    	if(oldEstado1 == HIGH && estado1 == LOW){
-      		oldEstado1 = estado1;
-  	  		oldEstado2 = estado2;
-      	return -1;
-        }
-    }
-  }
+  //Serial.println(estado1);
+  //Serial.println(estado2); 
+   if(oldEstado1 == LOW && oldEstado2 == HIGH && estado1 == LOW && estado2 == LOW){
+     oldEstado1 = estado1;
+     oldEstado2 = estado2;
+     Serial.println(estado1);
+     Serial.println(estado2);
+     Serial.println(oldEstado1);
+     Serial.println(oldEstado2);
+     return -1; //caso rotacao antihoraria
+     }
+     if(oldEstado1 == HIGH && oldEstado2 == LOW && estado1 == LOW && estado2 == LOW){
+     oldEstado1 = estado1;
+     oldEstado2 = estado2;
+     Serial.println(estado1);
+     Serial.println(estado2);
+     Serial.println(oldEstado1);
+     Serial.println(oldEstado2);
+     return 1; //caso rotacao antihoraria
+     }
   oldEstado1 = estado1; 
   oldEstado2 = estado2;
+  }
   return 0; //caso sem mudanca de estado
 }
